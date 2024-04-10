c4a145b5b0ccb0115d6126ff33887591d7887d6b
lutz <luocong2016@126.com>
2024-04-10 11:00:04 +0800

    feat: 地图飞线

--- packages/vue3/src/pages/demo/threejs/China.jsx
+++ packages/vue3/src/pages/demo/threejs/China.jsx
@@ -82,7 +82,7 @@ export default defineComponent({
           bevelEnabled: false,
         })
         const material = new THREE.MeshBasicMaterial({
-          color: color,
+          color,
           transparent: true,
           opacity: 0.5,
         })
@@ -147,18 +147,161 @@ export default defineComponent({
               province.add(mesh)
             })
           }
-          console.log(`${properties.name} province: `, province)
+
+          // console.log(`${properties.name} province: `, province)
 
           map.add(province)
         })
 
-        console.log("map:", map)
+        // console.log("map:", map)
 
         scene.add(map)
       }
 
+      /**
+       * 两点链接飞线
+       * */
+      function lineConnect(posStart, posEnd) {
+        // 根据目标坐标设置3D坐标  z轴位置在地图表面
+        const [x0, y0, z0] = [...posStart, 10.01]
+        const [x1, y1, z1] = [...posEnd, 10.01]
+
+        // 使用QuadraticBezierCurve3() 创建 三维二次贝塞尔曲线
+        const curve = new THREE.QuadraticBezierCurve3(
+          new THREE.Vector3(x0, -y0, z0),
+          new THREE.Vector3((x0 + x1) / 2, -(y0 + y1) / 2, 20),
+          new THREE.Vector3(x1, -y1, z1)
+        )
+
+        // 绘制 目标位置
+        spotCircle([x0, y0, z0])
+        spotCircle([x1, y1, z1])
+        moveSpot(curve)
+
+        const lineGeometry = new THREE.BufferGeometry()
+        // 获取曲线 上的50个点
+        const points = curve.getPoints(50)
+        const positions = []
+        const colors = []
+        const color = new THREE.Color()
+
+        // 给每个顶点设置演示 实现渐变
+        for (let j = 0; j < points.length; j++) {
+          color.setHSL(0.81666 + j, 0.88, 0.715 + j * 0.0025) // 粉色
+          colors.push(color.r, color.g, color.b)
+          positions.push(points[j].x, points[j].y, points[j].z)
+        }
+
+        // 放入顶点 和 设置顶点颜色
+        lineGeometry.setAttribute(
+          "position",
+          new THREE.BufferAttribute(new Float32Array(positions), 3, true)
+        )
+
+        lineGeometry.setAttribute(
+          "color",
+          new THREE.BufferAttribute(new Float32Array(colors), 3, true)
+        )
+
+        const material = new THREE.LineBasicMaterial({
+          vertexColors: true,
+          side: THREE.DoubleSide,
+        })
+        const line = new THREE.Line(lineGeometry, material)
+
+        return line
+      }
+
+      // 圆环网格对象组
+      const circleYs = []
+      /**
+       * 目标位置
+       * */
+      function spotCircle(spot) {
+        // 圆
+        const geometry1 = new THREE.CircleGeometry(0.5, 200)
+        const material1 = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide })
+        const circle = new THREE.Mesh(geometry1, material1)
+        // 绘制地图时 y轴取反 这里同步
+        circle.position.set(spot[0], -spot[1], spot[2] + 0.1)
+        scene.add(circle)
+
+        // 圆环
+        const geometry2 = new THREE.RingGeometry(0.5, 0.7, 50)
+        // transparent 设置 true 开启透明
+        const material2 = new THREE.MeshBasicMaterial({
+          color: 0xff0000,
+          side: THREE.DoubleSide,
+          transparent: true,
+        })
+        const circleY = new THREE.Mesh(geometry2, material2)
+        // 绘制地图时 y轴取反 这里同步
+        circleY.position.set(spot[0], -spot[1], spot[2] + 0.1)
+        scene.add(circleY)
+
+        circleYs.push(circleY)
+      }
+
+      // 移动物体网格对象组
+      const moveSpots = []
+      /**
+       * 线上移动物体
+       * */
+      function moveSpot(curve) {
+        // 线上的移动物体
+        const aGeo = new THREE.SphereGeometry(0.4, 0.4, 0.4)
+        const aMater = new THREE.MeshPhongMaterial({ color: 0xff0000, side: THREE.DoubleSide })
+        const aMesh = new THREE.Mesh(aGeo, aMater)
+        // 保存曲线实例
+        aMesh.curve = curve
+        aMesh._s = 0
+
+        moveSpots.push(aMesh)
+
+        scene.add(aMesh)
+      }
+
+      const line = lineConnect(
+        projection([106.557691, 29.559296]),
+        projection([121.495721, 31.236797])
+      )
+      scene.add(line)
+      const line2 = lineConnect(
+        projection([106.557691, 29.559296]),
+        projection([104.006215, 30.650055])
+      )
+      scene.add(line2)
+      const line3 = lineConnect(
+        projection([106.557691, 29.559296]),
+        projection([116.396795, 39.93242])
+      )
+      scene.add(line3)
+      const line4 = lineConnect(
+        projection([106.557691, 29.559296]),
+        projection([87.617099, 43.863737])
+      )
+      scene.add(line4)
+
       // 渲染
       function render() {
+        circleYs.forEach(function (mesh) {
+          //  目标圆环放大并透明
+          mesh._s += 0.01
+          mesh.scale.set(1 * mesh._s, 1 * mesh._s, 1 * mesh._s)
+          if (mesh._s <= 2) {
+            mesh.material.opacity = 2 - mesh._s
+          } else {
+            mesh._s = 1
+          }
+        })
+
+        moveSpots.forEach(function (mesh) {
+          mesh._s += 0.006
+          let tankPosition = new THREE.Vector3()
+          tankPosition = mesh.curve.getPointAt(mesh._s % 1)
+          mesh.position.set(tankPosition.x, tankPosition.y, tankPosition.z)
+        })
+
         renderer.render(scene, camera)
         requestAnimationFrame(render)
       }
